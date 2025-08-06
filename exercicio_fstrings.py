2d070f4f14e354aed031527351a0a23d6bbf4a4f
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2025-08-06 08:55:07 -0300

    Criar exercicio_fstrings.py

---
+++ exercicio_fstrings.py
@@ -0,0 +1,17 @@
+#f strings
+
+nome= "Drizzt"
+sobrenome= "d Urden"
+idade= 87
+raca = "drow"
+classe = "ranger"
+dano = 8000
+forte = dano >= 3000
+
+
+
+print("LENDAS")
+
+frase_1 = f'O nome dele é {nome} {sobrenome} viveu no subterrâneo por muito tempo ate sair daquele inferno aos 30 anos, hoje com seus {idade} considerado jovem devido sua raça {raca} é considerado uma lenda {classe} por todos.'
+
+print(frase_1)
