b4319b6e9a446647bb3c4d1cee206e718f25e8d4
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2025-08-08 20:00:45 -0300

    Criar input.py

---
+++ input.py
@@ -0,0 +1,13 @@
+nome = input('qual é seu nome ?')
+idade = int(input('qual é sua idade?'))
+altura =float(input('quL é sua altura ?'))
+
+print(f'O nome é {nome} a sua idade é {idade} e sua altura é {altura}')
+
+num1 = input('Digite o primeiro numero')
+num2 = input('Digite o segundo mumero')
+
+
+n1 = int(num1)
+n2 = int(num2)
+print(f'A soma é {n1 + n2}')
