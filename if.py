c7662a8d84788239e947610a8bd3849d8dc49248
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2025-08-11 19:32:14 -0300

    Criar if.py

---
+++ if.py
@@ -0,0 +1,9 @@
+usuario = input('digite o usuario.')
+senha =input('digite a senha:')
+
+
+
+if usuario == 'debsoam' and senha == '123':
+	print('entrou')
+else:
+	print(usuário ou senha incorreta.')
