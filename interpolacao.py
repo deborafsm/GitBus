9873f79b4655372f83da66e5202d23bba155b900
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2025-08-15 08:51:59 -0300

    Criar interpolacao.py

---
+++ interpolacao.py
@@ -0,0 +1,15 @@
+"""
+interpolação
+s = string
+d e i = int
+f = float
+x e x hexadecimal
+"""
+
+
+produto = 'maçã'
+preco = 19.99
+variavel = 'Preço: %s R$%d' % (produto, preco)
+print(variavel)
+
+print('O hexadecimal de %d é %00X'%(1500,1500))
