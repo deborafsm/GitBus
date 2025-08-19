a7cdbb59cb85a541225da1a6e6ac50832f032815
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2025-08-19 19:44:40 -0300

    Criar strings.py

---
+++ strings.py
@@ -0,0 +1,8 @@
+#f string
+variavel = 'Debora'
+print(f'{variavel: >10}')
+#fatiamrnto de string
+#por onde começar:qntas letrs #tem:pular quantas letras 
+print(variavel[0:6:3])
+#saber quantas letras tem
+print(len(variavel))
