803b16ca47966256d4940a2116edd2f1c0573d45
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2025-09-02 20:08:14 -0300

    Atualizar o cent_days_exercicio_day3.py

--- cent_days_exercicio_day3.py
+++ cent_days_exercicio_day3.py
@@ -1,3 +1,5 @@
+valor_total = 0
+
 altura = input(("você tem 1.20 de altura ?"))
 
 if "sim":
@@ -8,15 +10,19 @@ else:
 idade = input("qual é a sua idade ?")
 
 if int(idade) < 12:
+	valor_total += 5
 	print("O ingresso 🎫 custa R$5,00")
 if int(idade) > 12 and int(idade) < 18:
+	valor_total += 7
 	print("o ingresso 🎫 custará R$7,00")
 else:
+	valor_total += 12
 	print("O ingresso 🎫 custará R$12.00")
 	
 photo = input("você quer tirar fotos ? sim ou não ")
 
 if "sim":
-	print("para tirar foto é")
+	valor_total += 10
+	print(f"para tirar foto é {valor_total}")
 else:
-	print("ok")
+	print(f"{preco}")
