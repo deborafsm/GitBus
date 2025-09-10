4161850a8239e25c69b2b980d886f8dccb4d8b6b
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2025-09-10 20:47:18 -0300

    Criar emitions.py

---
+++ emitions.py
@@ -0,0 +1,14 @@
+#var de escopo geraal
+
+def emotions():
+
+	print("Entre com a sua emocao de hoje")
+	print("1 Feliz 😄")
+	print("2 Triste 😢")
+	print("3 Mais ou menos 🫤")
+	print("4 Raiva 😡")
+
+
+emotions()
+emocao = int(input("digite a sua emocao de hoje"))
+
