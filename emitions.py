365458253757c60cbf1f5bf4048f5a0a7c396451
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2025-09-11 09:20:01 -0300

    Atualizar o emitions.py

--- emitions.py
+++ emitions.py
@@ -5,14 +5,28 @@ def emotions():
 	print("Entre com a sua emocao de hoje")
 	print("1 Feliz 😄")
 	print("2 Triste 😢")
-	print("3 Mais ou menos 🫤")
-	print("4 Raiva 😡")
+	print("3 Neutro 😐")
+	print("4 Chateado(a) 😡")
 
 
 emotions()
 emocao = int(input("digite a sua emocao de hoje"))
 
-if emocao == 1:
-	emocao_lista.append("Feliz 😄")
+def add_lista():
+	if emocao == 1:
+		emocao_lista.append("Feliz 😄")
+	elif emocao == 2:
+		emocao_lista.append("Triste 😢 ")
+	elif emocao == 3:
+		emocao_lista.append("Neutro 😐 ")
+	elif emocao == 4:
+		emocao_lista.append("Chateado(a) 😡")
+	else:
+		print("opção inválida.")
 	
+		
+			
+					
+add_lista()
 print(emocao_lista)
+	
