90448233bf05fe74c8f4fc20aa935f20286fc6f2
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2025-09-17 09:59:33 -0300

    Criar esmaltes.py

---
+++ esmaltes.py
@@ -0,0 +1,22 @@
+#cor de esmalte de cada semana.
+
+
+#lista das cores e cada semana
+semana_cores =[]
+
+cores ={
+	1: "Roxo",
+	2: "Preto",
+	3: "Azul",
+	4: "Prata",
+	5: "Verde"
+}
+
+semana ={
+
+	1:"1ª semana",
+	2:"2ª semana",
+	3:"3ª semana",
+	4:"4ª semana",
+	5:"5ª semana"
+}
