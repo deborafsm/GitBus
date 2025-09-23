0d6822cc334c246f577e68e34c47f688c9bc0657
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2025-09-23 20:06:09 -0300

    Criar pocoes.py

---
+++ intermediario/pocoes.py
@@ -0,0 +1,28 @@
+:'''
+Remur chegou na loja de poções de um amigo que ele conhecia a mais de 200 anos.
+o amigo lhe deu boas vindas!
+Remur o comprimentou e olhou para a prateleira..ele observou mais de perto e já sabia qual iria pegar, poção de salgueiro para melhorar suas poções, poção amerallda para poçoes de anestésico e pocao ametista para criacao de poções que curavam urticária.
+Remur - hoje vou levar a poção salgueiro, amerallda e ametista.
+Elgar - quer levar outra ?
+Remur - só isso por hoje meu amigo, me passe o valor.
+Elgar disse o valor.
+Remur - Deve esta tão caro assim por causa da amerallda ... 
+Elgar - é amerallda é sempre mais caro, e os anoes sempre aumentam o preco.
+'''
+
+prateleira = []
+
+pocoes ={
+	1:"Amerallda",
+	2:"Salgueiro",
+	3:"Ametista"
+}
+preco ={
+	1:"33 ouros 20 pratas",
+	2:"20 ouro e 80 pratas ",
+	3:"25 ouro 40 pratas"
+}
+
+
+
+pedido
