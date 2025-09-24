12c73ed45fe96fbd3c950b6b2b7bc8965343777b
Débora Freire <62457927+deborafsm@users.noreply.github.com>
2025-09-24 19:42:09 -0300

    Atualizar o pocoes.py

--- intermediario/pocoes.py
+++ intermediario/pocoes.py
@@ -1,4 +1,4 @@
-:'''
+'''
 Remur chegou na loja de poções de um amigo que ele conhecia a mais de 200 anos.
 o amigo lhe deu boas vindas!
 Remur o comprimentou e olhou para a prateleira..ele observou mais de perto e já sabia qual iria pegar, poção de salgueiro para melhorar suas poções, poção amerallda para poçoes de anestésico e pocao ametista para criacao de poções que curavam urticária.
@@ -13,12 +13,17 @@ Elgar - é amerallda é sempre mais caro, e os anoes sempre aumentam o preco.
 bolsa = []
 
 pocoes = {
-	1: {"nome":"Amerallda","preco":50.20}
-	2: {"nome":"Salgueiro","preco":20.30}
-	3: {"nome":"Ametista", "preco":30.40}
+	1: {"nome":"Amerallda","preco":50.20},
+	22: {"nome":"Salgueiro","preco":20.30},
+	34: {"nome":"Ametista", "preco":30.40}
+}
 
 
-pedido = int(input("Oque vai ser hoje amigo ?"))
+#pedido = int(input("Oque vai ser hoje amigo ?"))
 
+def mostra_menu():
+	print("Menu")
+	for prateleira, item in pocoes.items():
+		print(f"{prateleira}")
 
-def 
+mostra_menu()
